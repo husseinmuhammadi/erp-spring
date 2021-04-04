0899bbe729d67c54a2121bd10eb7397b853f5b05
Hossein Mohammadi <hussein.muhammadii@gmail.com>
2021-04-04 17:52:09 +0430

    Spring Security

--- web-api/src/main/java/com/digiboy/erp/web/security/WebSecurityConfig.java
+++ web-api/src/main/java/com/digiboy/erp/web/security/WebSecurityConfig.java
@@ -24,7 +24,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         http.authorizeRequests()
                 .antMatchers("/login").permitAll()
                 .anyRequest().authenticated()
-                .and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+                .and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.ALWAYS);
     }
 
     @Autowired
