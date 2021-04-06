64a93ae3331d18295e4efdbb998f9d08317822bf
Hossein Mohammadi <hossein.mohammadi@outlook.com>
2021-04-06 09:14:34 +0430

    Create token on user login

--- init.sh
+++ init.sh
@@ -2,11 +2,11 @@
 set -e
 
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
-    CREATE USER eap WITH PASSWORD '123456';
-    CREATE DATABASE eap;
-    GRANT ALL PRIVILEGES ON DATABASE eap TO eap;
+    CREATE USER firmco WITH PASSWORD '123456';
+    CREATE DATABASE firmco;
+    GRANT ALL PRIVILEGES ON DATABASE firmco TO firmco;
 EOSQL
 
-psql -v ON_ERROR_STOP=1 --username "eap" --dbname "eap" <<-EOSQL
-    CREATE SCHEMA eap;
+psql -v ON_ERROR_STOP=1 --username "firmco" --dbname "firmco" <<-EOSQL
+    CREATE SCHEMA master;
 EOSQL
