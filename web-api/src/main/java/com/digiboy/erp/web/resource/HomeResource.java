d2c872d656f201b6030fcca276e5c79eb0253021
Hossein Mohammadi <hossein.mohammadi@outlook.com>
2021-04-07 22:43:57 +0430

    Dockerize project

--- web-api/src/main/java/com/digiboy/erp/web/resource/HomeResource.java
+++ web-api/src/main/java/com/digiboy/erp/web/resource/HomeResource.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HomeResource {
 
-    @GetMapping("/home")
+    @GetMapping({"/", "/home", "/index"})
     @ResponseBody
     public String index() {
         return "Server is up!";
