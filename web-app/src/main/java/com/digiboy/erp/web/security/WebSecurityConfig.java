fc2961fd3138055fe828d9a09f189a4b8837ff63
Hossein Mohammadi <hossein.mohammadi@specific-group.com>
2021-05-25 08:38:00 +0430

    Update security for successful handler

--- web-app/src/main/java/com/digiboy/erp/web/security/WebSecurityConfig.java
+++ web-app/src/main/java/com/digiboy/erp/web/security/WebSecurityConfig.java
@@ -5,19 +5,28 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
     UserDetailsService userDetailsService;
+
+    @Autowired
+    private SimpleAuthenticationSuccessHandler successHandler;
+
+    @Override
+    public void configure(WebSecurity security) {
+        security.ignoring().antMatchers("/css/**", "/images/**", "/js/**");
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -30,10 +39,11 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers("/admin/**").hasRole("ADMIN")
                 .anyRequest().authenticated()
                 .and()
-                .formLogin().loginPage("/login").permitAll()
+                .formLogin().loginPage("/login").successHandler(successHandler).permitAll()
                 .and()
-                .sessionManagement()
-                .sessionCreationPolicy(SessionCreationPolicy.ALWAYS);
+                .logout().logoutSuccessUrl("/")
+                .and()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.ALWAYS);
     }
 
     @Autowired
